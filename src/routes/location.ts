b36193282fe4683a6004fbcc6f8be041085ae6a1
Smart  Raysam <engsamtob@yahoo.com>
2024-11-13 07:56:43 +0100

    Refactor location routes to switch from POST to GET for retrieving locations; enhance Swagger documentation and improve error handling for location creation

--- src/routes/location.ts
+++ src/routes/location.ts
@@ -22,72 +22,44 @@ const locationRoutes = Router();
 
 /**
  * @swagger
- * /api/locations/count:
- *   get:
- *     summary: Get the total count of locations
- *     tags: [Locations]
- *     security:
- *       - bearerAuth: []
- *     responses:
- *       200:
- *         description: The total count of locations
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 count:
- *                   type: integer
- *       500:
- *         description: Internal server error.
- */
-locationRoutes.get("/locations/count", async (req: Request, res: Response) => {
-  try {
-    const count = await getLocationCount();
-    res.status(200).json({ count });
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-/**
- * @swagger
  * /api/locations:
- *   get:
- *     summary: Retrieve all locations
+ *   post:
+ *     summary: Create new location
  *     tags: [Locations]
  *     security:
- *       - bearerAuth: []
+ *      - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *            type: object
+ *           properties:
+ *            address:
+ *             type: string
+ *            name:
+ *             type: string
+ *            adminId:
+ *             type: integer
+ *            logo:
+ *              type: string
  *     responses:
  *       200:
- *         description: List of locations
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                   address:
- *                     type: string
- *                   name:
- *                     type: string
- *                   adminId:
- *                     type: object
- *                   email:
- *                     type: string
+ *         description: Location updated successfully.
+ *       404:
+ *         description: Location not found.
  *       500:
  *         description: Internal server error.
  */
-locationRoutes.get("/locations", async (req: Request, res: Response) => {
+locationRoutes.post("/locations", async (req: Request, res: Response) => {
   try {
-    const locations = await getLocations();
-    res.status(200).json(locations);
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    await createNewLocation(req.body);
+    res.status(201).json({ message: "Location created successfully" });
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
   }
 });
+
 /**
  * @swagger
  * /api/locations/user:
@@ -161,47 +133,75 @@ locationRoutes.delete("/locations/delete/:id", async (req: Request, res: Respons
   }
 });
 
+/**
+ * @swagger
+ * /api/locations/count:
+ *   get:
+ *     summary: Get the total count of locations
+ *     tags: [Locations]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: The total count of locations
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 count:
+ *                   type: integer
+ *       500:
+ *         description: Internal server error.
+ */
+locationRoutes.get("/locations/count", async (req: Request, res: Response) => {
+  try {
+    const count = await getLocationCount();
+    res.status(200).json({ count });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 /**
  * @swagger
  * /api/locations:
- *   post:
- *     summary: Create new location
+ *   get:
+ *     summary: Retrieve all locations
  *     tags: [Locations]
  *     security:
- *      - bearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         multipart/form-data:
- *           schema:
- *            type: object
- *           properties:
- *            address:
- *             type: string
- *            name:
- *             type: string
- *            adminId:
- *             type: integer
- *            logo:
- *              type: string
+ *       - bearerAuth: []
  *     responses:
  *       200:
- *         description: Location updated successfully.
- *       404:
- *         description: Location not found.
+ *         description: List of locations
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   address:
+ *                     type: string
+ *                   name:
+ *                     type: string
+ *                   adminId:
+ *                     type: object
+ *                   email:
+ *                     type: string
  *       500:
  *         description: Internal server error.
  */
-locationRoutes.post("/locations", async (req: Request, res: Response) => {
+locationRoutes.get("/locations", async (req: Request, res: Response) => {
   try {
-    await createNewLocation(req.body);
-    res.status(201).json({ message: "Location created successfully" });
-  } catch (error: any) {
-    res.status(400).json({ message: error.message });
+    const locations = await getLocations();
+    res.status(200).json(locations);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
   }
 });
-
 
 /**
  * @swagger
